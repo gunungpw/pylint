c89e6ec09ef454d3421c2cc83c850674fbc5b3aa
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-04-02 14:59:23 +0200

    Add a functional test for the spelling checker

---
+++ tests/functional/s/spelling/spelling.py
@@ -0,0 +1,6 @@
+"""Functional test for speling.""" # [wrong-spelling-in-docstring]
+
+# mispeled text # [wrong-spelling-in-comment]
+
+def function_name():
+    """mispeled text.""" # [wrong-spelling-in-dostring]
